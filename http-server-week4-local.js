ec7f45ffc517b9232648d75f1b2088bac969b8d2
Luc Moreau <luc.moreau@kcl.ac.uk>
2022-10-18 09:18:43 +0100

    local version

---
+++ http-server-week4-local.js
@@ -0,0 +1,69 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 9090));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+});
+
+app.counters={count:0};
+
+function hostname_and_port(request) {
+    return (request.hostname || request.ip).replace("::ffff:","") + ":" + (app.get('port'));
+}
+
+app.post('/resource', function(request, response) {
+    console.log("post");
+    app.counters.count=app.counters.count+1;
+    const v=app.counters.count;
+    response.redirect(303, request.protocol + "://" + hostname_and_port(request) + request.path + "/" + v);
+});
+
+app.get('/resource', function(request, response) {
+    console.log("get");
+    response.send( "<html>" +
+        " <form action='/resource' method='post'>\n" +
+        "  <input type='submit' value='Create Resource'>\n" +
+        "</form> \n" +
+        "<p/>" +
+        "<a href='/resource'>Go back to form</a>" +
+        "</html>\n")
+});
+
+
+app.get('/resource/:id', function(request, response) {
+    let id = request.params.id;
+    console.log("get "+ id);
+
+
+    response.format({
+
+        'text/html': function () {
+            response.send('<p>resource indexed: ' + id + '</p>'  +
+            "<a href='/resource'>Go back to form</a>" );
+        },
+
+        'application/json': function () {
+            response.send({resource: id})
+        },
+
+        default: function () {
+            // log the request and respond with 406
+            response.status(406).send('Not Acceptable')
+        }
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+
+
+app.get('/exit', function(request, response) {
+    response.send('Exiting!')
+    process.exit(0);
+})
+
